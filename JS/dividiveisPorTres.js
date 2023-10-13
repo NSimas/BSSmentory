0374e9df2a1d327b6d2ee191332649f81f14a11e
Nélia Simas <neliasimas@hotmail.com>
2023-10-13 17:16:17 -0300

    ok

---
+++ JS/dividiveisPorTres.js
@@ -0,0 +1,12 @@
+// usando loops e operações condicionais, identificar quais números 
+//entre 0 e 10.000 são múltiplos de 3 (ou divisíveis por 3).
+
+let i = 0
+
+while (i < 10000) {
+    if ((i % 3) === 0){
+        console.log(`${i} é múltiplo de 3`)
+    } else {
+    }
+    i=i++
+}
